131e39520139a3771f5526aa57b9e59fa242f531
stella <stellaaugustalins@gmail.com>
2022-03-10 21:05:05 -0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { CardComponent } from './card/card.component';
+import { ToogleComponent } from './toogle/toogle.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CardComponent,
+    ToogleComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/toogle/toogle.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-toogle',
+  templateUrl: './toogle.component.html',
+  styleUrls: ['./toogle.component.css']
+})
+export class ToogleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
